166901a2a411d52450dd756e0de08d0874785e6f
Christina Gerard <ChristinaGerard@Christinas-MBP.attlocal.net>
2018-06-24 13:32:32 -0400

    Final Draft

---
+++ assets/javascript/game.js
@@ -0,0 +1,151 @@
+var wins = 0;
+var tries = 10;
+
+var wordList = {
+    word1: "titanic",
+    word2: "friday",
+    word3: "clueless",
+    word4: "swingers",
+    word5: "terminator",
+    word6: "matrix",
+    word7: "juice",
+    word8: "scream",
+    word9: "fargo",
+    word10: "heat",
+    word11: "goodfellas",
+    word12: "seven"
+};
+
+var wordOptions = [wordList.word1, wordList.word2, wordList.word3, wordList.word4,
+wordList.word5, wordList.word6, wordList.word7, wordList.word8, wordList.word9,
+wordList.word10, wordList.word11, wordList.word12];
+
+function setUpEvents() {
+
+    //Variables
+
+    var randomWord;
+    var userGuess = "";
+    var dash = "";
+    var underscore = [];
+    var scoreAmount;
+    var answer = "";
+    var keys = [];
+    var letters = [];
+    var splice = "";
+    var final = "";
+    var last = keys[keys.length - 1];
+    var indexTwo = [];
+    var indexOne;
+    var keyLength = 1;
+
+    var push = indexTwo.push(i);
+
+    //For loop Variables
+    var i;
+    var j;
+    var k;
+    var l;
+    var s;
+
+    //Scoreboard
+    var tW;
+    tW = wins + 1;
+
+    // Random Word Picker
+    randomWord = wordOptions[Math.floor(Math.random() * wordOptions.length)];
+
+    console.log(randomWord);
+
+    //Underscore length
+    for (k = 0; k < randomWord.length; k++) {
+        dash = "_";
+        underscore.push(dash);
+    }
+
+    //Underscore Output
+    document.getElementById("game").innerHTML = underscore.join('  ');
+
+    //Tries
+    document.getElementById("tries").innerHTML = tries;
+
+    // Wins
+    document.getElementById("wins").innerHTML = wins;
+
+    //on key up function
+    document.onkeyup = function (event) {
+
+        userGuess = event.key;
+
+        //No duplicate letters
+        function letterChecks(arr) {
+            var keyArray = []
+            for (l = 0; l < arr.length; l++) {
+                if (keyArray.indexOf(arr[l]) == -1) {
+                    keyArray.push(arr[l])
+                }
+            }
+            return document.getElementById("letters").innerHTML = keyArray.join('  ');
+        }
+
+        keys.push(event.key);
+        letterChecks(keys);
+
+        for (i = 0; i < randomWord.length; i++) {
+            document.getElementById("tries").innerHTML = tries;
+            
+            if (userGuess == randomWord.charAt(i)) {
+
+                underscore[i] = userGuess;
+                console.log("166: i: " + i);
+                
+                answer = underscore.join("  ");
+
+                document.getElementById("game").innerHTML = answer;
+
+                if (underscore.indexOf("_") >= 0) {
+
+                } else {
+                    wins++;
+                    console.log("303: wins: " + wins);
+                    document.getElementById("wins").innerHTML = wins;
+                    randomWord = wordOptions[Math.floor(Math.random() * wordOptions.length)];
+
+                    tries = 10;
+
+                    setUpEvents();
+
+                }
+
+            }
+            last = keys[keys.length - 1];
+
+        }
+
+        //Tries decremented
+        if (userGuess !== underscore[randomWord.indexOf(userGuess)]) {
+            if (underscore.indexOf(userGuess) === -1) {
+                tries--;
+                document.getElementById("tries").innerHTML = tries;
+                console.log(tries);
+
+                if (tries === 0) {
+                    
+                    tries = 10;
+                    setUpEvents();
+                }
+            }
+        }
+
+    };
+}
+
+for (q = 0; q < (wordOptions.length + 10); q++) {
+    window.onload = function () {
+        setUpEvents();
+    };
+
+}
+
+
+
